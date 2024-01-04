46c82e43fea9ef1d1ee71e1c641ce958e3beaa6f
Milmon <107737161+Molmin@users.noreply.github.com>
2024-01-04 17:04:30 +0800

    feat(template): init `solve` function

--- src/template.cpp
+++ src/template.cpp
@@ -1,8 +1,24 @@
+/*
+ * @type: checker
+ * @author: Milmon <milmon@qq.com>
+ * @date: 2024-01-01
+ */
+
 #include<bits/stdc++.h>
 #include"testlib.h"
 using namespace std;
 
+bool solve(InStream& stream){
+    string res=stream.readToken();
+    if(res=="No")return false;
+    return true;
+}
+
 int main(int argc,char** argv){
     registerTestlibCmd(argc,argv);
+    int out=solve(ouf),answer=solve(ans);
+    if(out==answer)quitf(_ok,"Correct");
+    else if(answer)quitf(_wa,"Wrong Answer");
+    else quitf(_fail,"Failed");
     return 0;
 }
